253068afceebdfa9c97371bb866dc0a2d86bd3d1
“chengdegong” <chengdegong@weizoom.com>
2016-09-27 16:07:00 +0800

    “chengdg”

--- features/steps/product_steps.py
+++ features/steps/product_steps.py
@@ -17,3 +17,16 @@ def step_impl(context, user, product_id):
     expected = json.loads(context.text)
 
     bdd_util.assert_list(expected, actual_product)
+
+@When(u"{user}调用'商品列表'api")
+def step_impl(context, user):
+	user_id = bdd_util.get_user_id_for(user)
+	response = context.client.get('/mall/products/', {'woid': user_id})
+	print  "========================================", repr(response.content)
+	context["actual_product_list"] = json.loads(response.content)['data']['items']
+
+
+@Then(u"{user}获取'商品列表'api返回结果")
+def step_impl(context, user, product_id):
+    expected = json.loads(context.text)
+    bdd_util.assert_list(expected, context["actual_product_list"])
