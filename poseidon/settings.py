1e536ea6ce97f4a115ada7ba7d5406f37b2a9ac5
郭玉成 <guoyucheng@weizoom.com>
2016-09-28 14:29:33 +0800

    update mns setting

--- poseidon/settings.py
+++ poseidon/settings.py
@@ -258,6 +258,7 @@ else:
     MNS_ACCESS_KEY_ID = 'eJ8LylRwQERRqOot'
     MNS_ACCESS_KEY_SECRET = 'xxPrfGcUlnsL7IPweJRqVekHTCu6Ad'
     MNS_ENDPOINT = 'http://1615750970594173.mns.cn-hangzhou.aliyuncs.com/'
+    MNS_SECURITY_TOKEN = ''
     #MNS_ENDPOINT = 'http://16
 
 UPLOAD_DIR = os.path.join(PROJECT_HOME, '../static', 'upload') #文件上传路径
